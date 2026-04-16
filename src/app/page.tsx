af7f22fcf2dc234c65724122efaed18946984aa3
Pronoy <pronoypaul56@gmail.com>
2026-04-17 00:39:58 +0600

    splash screen created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import SplashScreen from "@/components/splashScreen/SplashScreen";
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  // redirect("/login");
+
   return <SplashScreen />;
 }
